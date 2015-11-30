aeda3da416c460f2cd0b9d2019cafb173938d689
Jeff Ely <jeff.ely.08@gmail.com>
2015-11-30 16:19:14 -0500

    added py folder for OpenPIV python code

---
+++ py/check_dependencies.py
@@ -0,0 +1,14 @@
+__author__ = 'Jwely'
+
+import pip
+import os
+
+# just change this directory to the local filepath of the resource folder
+wheel_dir = r"C:\Users\Jeff\Downloads\python27_64bit_resources"
+
+os.system(os.path.join(wheel_dir, r"VCForPython27.msi"))
+pip.main(["install", "--upgrade", "pip"])
+pip.main(["install", "openpiv"])
+
+
+
