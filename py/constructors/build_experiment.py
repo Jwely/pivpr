015a9cad2e9875e882eb13c2a4a5f10dc2ac8051
Jeff Ely <jeff.ely.08@gmail.com>
2015-12-03 18:20:54 -0500

    filled methods on experiment class
    enough is finished to go ahead and comput binary pickles for all of the
    test cases. there is a test case with a bad calibration that needs some
    manual adjustment that is not taken into account yet.

--- py/constructors/build_experiment.py
+++ py/constructors/build_experiment.py
@@ -1,15 +1,55 @@
 __author__ = 'Jwely'
 
+import pandas as pd
+import os
+from py.managers.Experiment import Experiment
+from build_axial_vortex import build_axial_vortex
 
-def build_experiment(experiment_id):
+
+def build_experiments(experiment_table_path, experiment_directory_path, ids=None):
     """
     Constructs an Experiment instance with all useful attributes of the experiment. Some of these
     attributes are read from ancillary data in the `dat` folder.
 
-    :param experiment_id:   integer between 1 and 70 corresponding to the experiment ID
-    :return Experiment:
+    :param experiment_table_path:
+    :param experiment_directory_path:
+    :param ids:                         list of run ID numbers to use
+    :return:
     """
-    pass
+
+    dataframe = pd.read_csv(experiment_table_path)
+    experiments = []
+
+    if ids is None:
+        ids = range(0, len(dataframe) + 1)
+
+    for i, row in dataframe.iterrows():
+        if row['experiment_id'] in ids:
+            # build the experiment object
+            kwargs = row.to_dict()
+            exp_dir = os.path.join(experiment_directory_path, "1")
+            exp = Experiment(experiment_dir=exp_dir, **kwargs)
+
+            # now build up the vortex associated with it, and add it to the experiment
+            name_tag = "ID-{0}_Z-{1}_Vfs-{2}".format(row['experiment_id'],
+                                                     row['z_location'],
+                                                     row['v_fs_mean'])
+
+            av = build_axial_vortex(exp_dir, "../pickles",
+                                    name_tag=name_tag,
+                                    include_dynamic=False,
+                                    velocity_fs=row['v_fs_mean'],
+                                    force_recalc=False)
+            exp.ingest_axial_vortex(av)
+            experiments.append(exp)
+
+    return experiments
+
+
+if __name__ == "__main__":
+
+    build_experiments("dat/experiment_table.csv", "../../data_full")
+
 
 
 
--- py/managers/Experiment.py
+++ py/managers/Experiment.py
@@ -1,14 +1,64 @@
 __author__ = 'Jwely'
 
+from py.utils.get_rel_humidity import get_rel_humidity
+
 
 
 class Experiment:
 
-    def __init__(self, experiment_id):
+    def __init__(self, experiment_dir, experiment_id, n_samples, z_location, v_nominal, dt, test_date,
+                 v_fs_mean, v_fs_sigma, q, pres_atm, temp_tunnel, wet_bulb, dry_bulb):
         """
+        Class to represent the entire state of an experiment
 
-        :param experiment_id:
+        :param experiment_dir:  filepath to directory containing experiment folders (str)
+        :param experiment_id:   integer ID number 1 through 70 (integer)
+        :param n_samples:       number of observations, usually 200 (integer)
+        :param z_location:      downstream distance of interrogation plane (inches, float)
+        :param v_nominal:       nominal velocity target (m/s float)
+        :param dt:              time between laser pulses (microseconds integer)
+        :param test_date:       date on which the experiment took place (datetime)
+        :param v_fs_mean:       measured free stream velocity (m/s float)
+        :param v_fs_sigma:      instability in free stream velocity (m/s float)
+        :param q:               stagnation pressure inside the tunnel (Pascals integer)
+        :param pres_atm:        atmospheric pressure (Pascals integer)
+        :param temp_tunnel:     temperature in the tunnel (Celcius float)
+        :param wet_bulb:        wet bulb temperature at time of test (Kelvin float)
+        :param dry_bulb:        wet bulb temperature at time of test (Kelvin float)
         :return:
         """
 
+        # attributes as arguments (must have been recorded at time of experiment)
         self.experiment_id = experiment_id
+        self.n_samples = n_samples
+        self.z_location = z_location
+        self.v_nominal = v_nominal
+        self.dt = dt
+        self.test_date = test_date
+        self.v_fs_mean = v_fs_mean
+        self.v_fs_sigma = v_fs_sigma
+        self.q = q
+        self.pres_atm = pres_atm
+        self.temp_tunnel = temp_tunnel
+        self.wet_bulb = wet_bulb
+        self.dry_bulb = dry_bulb
+
+        # calculate relative humidity from inputs
+        self.rel_humid = get_rel_humidity(dry_bulb, wet_bulb, pres_atm)
+
+        # attributes ingested by function call
+        self.axial_vortex = None        # will be an actual AxialVortex instance
+
+        # calculated and derived attributes from the axial vortex
+        self.core_radius = None         # distance between core and vmax_tangential location (mm)
+        self.vmax_tangential = None     # maximum tangential velocity
+        self.vmax_axial = None          # maximum axial velocity
+        self.vcore_axial = None         # axial velocity at the core
+
+
+    def ingest_axial_vortex(self, axial_vortex_instance):
+        """
+        :param axial_vortex_instance:
+        :return:
+        """
+        self.axial_vortex = axial_vortex_instance
