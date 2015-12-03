0b538ec280879c56f612296c3a2e2126a5aa3bf8
Jeff Ely <jeff.ely.08@gmail.com>
2015-12-02 20:16:39 -0500

    changed logical flow
    might as well produce both types of pickle files (dynamic and reduced)
    when either is computed.

--- py/constructors/build_axial_vortex.py
+++ py/constructors/build_axial_vortex.py
@@ -13,32 +13,38 @@ def build_axial_vortex(v3d_dir, pkl_dir, name_tag, include_dynamic=False,
     :param v3d_dir:         directory containing v3d files containing data on axial vortex
     :param pkl_dir:         directory containing binary pickle files
     :param name_tag:        a unique name identifier for the AxialVortex instance (used in pkl filename)
-    :param include_dynamic: set to True to keep matrix data from every individual run instead of taking averages
-                            and standard deviations then throwing out the individual datasets. Substantially
-                            increases system requirements to keep True.
+    :param include_dynamic: set to True to keep matrix data from every individual run instead of taking
+                            averages and standard deviations then throwing out the individual datasets.
+                            Substantially increases system requirements to keep True.
     :param velocity_fs:     The value of the free stream velocity in meters/second
     :param force_recalc:    set True to force re-computation of all parameters from raw datasets.
-    :return:
+
+    :return AxialVortex:
     """
 
     # generate pkl_path based on the pkl_dir and name_tag inputs.
+    pkl_path_dyn = os.path.join(os.path.abspath(pkl_dir), "{0}_dyn.pkl".format(name_tag))
+    pkl_path = os.path.join(os.path.abspath(pkl_dir), "{0}.pkl".format(name_tag))
+
     if include_dynamic:
-        pkl_path = os.path.join(os.path.abspath(pkl_dir), "{0}_dyn.pkl".format(name_tag))
+        if os.path.exists(pkl_path_dyn) and not force_recalc:
+            return AxialVortex().from_pickle(pkl_path_dyn)
     else:
-        pkl_path = os.path.join(os.path.abspath(pkl_dir), "{0}.pkl".format(name_tag))
+        if os.path.exists(pkl_path) and not force_recalc:
+            return AxialVortex().from_pickle(pkl_path)
 
+    # build list of all v3d files in directory from which to create the AxialVortex
+    v3d_paths = [os.path.join(v3d_dir, fname) for fname in os.listdir(v3d_dir) if fname.endswith(".v3d")]
+    av_instance = AxialVortex(name_tag=name_tag, v3d_paths=v3d_paths, velocity_fs=velocity_fs)
 
-    if os.path.exists(pkl_path) and not force_recalc:
-        av_instance = AxialVortex().from_pickle(pkl_path)
-        return av_instance
+    # find the core and build cylindrical coordinate data around it
+    av_instance.find_core()
+    av_instance.build_cylindrical()
 
-    else:
-        v3d_paths = [os.path.join(v3d_dir, fname) for fname in os.listdir(v3d_dir) if fname.endswith(".v3d")]
-        av_instance = AxialVortex(name_tag=name_tag, v3d_paths=v3d_paths, velocity_fs=velocity_fs)
-        av_instance.find_core()                           # find core location (not yet proven 100% robust)
-        av_instance.build_cylindrical()                   # convert cartesian data to cylindrical about core
-        av_instance.to_pickle(pkl_path, include_dynamic)  # pickle it so we can just load it again later
-        return av_instance
+    # pickle both the full dynamic set and the reduced set
+    av_instance.to_pickle(pkl_path_dyn, include_dynamic=True)
+    av_instance.to_pickle(pkl_path, include_dynamic=False)
+    return av_instance
 
 
 if __name__ == "__main__":
@@ -46,9 +52,9 @@ if __name__ == "__main__":
     kwargs = {"v3d_dir": r"E:\Data2\Ely_May28th\Vector\1",
               "pkl_dir": "../pickles",
               "name_tag": "01",
-              "include_dynamic": True,
+              "include_dynamic": False,
               "velocity_fs": 15.22,
               "force_recalc": False}
 
     av = build_axial_vortex(**kwargs)
-    av.show_contour('P')
+    av.get_contour_plot('P')
