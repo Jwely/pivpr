55d68779c7548f81eb19efd469b4f3c7c8de46fb
Jeff Ely <jeff.ely.08@gmail.com>
2015-12-02 22:47:49 -0500

    templating Experiment and constructor

---
+++ py/constructors/build_experiment.py
@@ -0,0 +1,11 @@
+__author__ = 'Jwely'
+
+
+def build_experiment(experiment_id):
+    """
+    constructs an Experiment instance with all useful attributes of the experiment. Some of these
+    attributes are read from ancillary data in the `dat` folder.
+
+    :param experiment_id:   integer between 1 and 70 corresponding to the experiment ID
+    :return Experiment:
+    """
---
+++ py/managers/Experiment.py
@@ -0,0 +1,14 @@
+__author__ = 'Jwely'
+
+
+
+class Experiment:
+
+    def __init__(self, experiment_id):
+        """
+
+        :param experiment_id:
+        :return:
+        """
+
+        self.experiment_id = experiment_id
