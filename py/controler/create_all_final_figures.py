ef7d92e4ae31d9cb0131ff9a69ed67416c5d24d2
Jeff Ely <jeff.ely.08@gmail.com>
2015-12-27 20:41:23 -0500

    docstrings

--- py/controler/create_all_final_figures.py
+++ py/controler/create_all_final_figures.py
@@ -15,20 +15,19 @@ def dynamic_import(module):
 
 
 def create_all_final_figures():
+    """
+    Iterates through every figure generating class in the list below. Take a look at the
+    FinalFigure class for better understanding of requirements for each entry in the
+    figs_class_list.
+    """
 
-    figs_class_list = ["ExamplePlots55.ExamplePlots55"]
+    figs_class_list = ["ExamplePlots55.ExamplePlots55",
+                       ]
 
     for fig_class_name in figs_class_list:
         fig_class = dynamic_import(fig_class_name)
         fig = fig_class()
         fig.generate()
-
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
