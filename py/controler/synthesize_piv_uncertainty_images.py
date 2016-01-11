bd84487e69b43fbefe27922dad41145e07f8fed1
Jeff Ely <jeff.ely.08@gmail.com>
2016-01-11 17:32:39 -0500

    renaming and refactoring

--- py/controler/synthesize_piv_uncertainty_images.py
+++ py/controler/synthesize_piv_uncertainty_images.py
@@ -2,9 +2,11 @@ __author__ = 'Jwely'
 
 import os
 from py.uncertainty import ArtificialPIV
+from py.config import *
+import copy
 
 
-def create_uncertainty_test_images():
+def synthesize_piv_uncertainty_images(uncertainty_image_dir):
     """
     Builds a bunch of test images for monte carlo uncertainty analysis.
 
@@ -12,12 +14,8 @@ def create_uncertainty_test_images():
     NOTE: This takes a while. The process of computing light intensities from particles
     and adding them to the final output image is probably very inefficient, but I don't know
     how to make it better and can't be bothered to figure it out. I'd rather just hit run and let
-    it go overnight.
-
-    :return:
+    it go over night (scratch that ... several days).
     """
-
-    uncertainty_image_dir = "../uncertainty/artificial_images"
 
     velocity_sets = {'high_z': {'dt': 25, 'x_vel': 0.00, 'y_vel': 0.00, 'z_vel': 40.0},  # high z
                      'low_z': {'dt': 40, 'x_vel': 0.00, 'y_vel': 0.00, 'z_vel': 10.0},  # low z
@@ -40,16 +38,20 @@ def create_uncertainty_test_images():
             if not os.path.exists(outdir):
                 os.makedirs(outdir)
 
-            cal_path = "../uncertainty/cal_data/station_{num}/ely_may28th.cal".format(num=cal)
+            cal_path = os.path.join(CALIBRATION_DIR, "station_{num}/ely_may28th.cal".format(num=cal))
             apiv = ArtificialPIV(dimensions, name)
             apiv.load_calibration_file(cal_path)
-            apiv.make_image_pairs(n_particles=dimensions[0] * dimensions[1] / 40,
-                                  output_dir=outdir,
-                                  particle_size=0.2,
-                                  particle_scatter=100,
-                                  light_sheet_thickness=3.0,
-                                  **velocity_sets[vel_key])
+
+            velocity_sets[vel_key].update({"n_particles": dimensions[0] * dimensions[1] / 40,
+                                           "output_dir": outdir,
+                                           "particle_size": 0.2,
+                                           "particle_scatter": 100,
+                                           "light_sheet_thickness": 3.0})
+
+            apiv.make_image_pairs(**velocity_sets[vel_key])
+
+
 
 
 if __name__ == "__main__":
-    create_uncertainty_test_images()
+    synthesize_piv_uncertainty_images("../uncertainty/artificial_images")
