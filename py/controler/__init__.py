28b4595d78aed273e16f2c9a717b57d3859ecfa1
Jeff Ely <jeff.ely.08@gmail.com>
2016-01-11 16:06:38 -0500

    added imports

--- py/controler/__init__.py
+++ py/controler/__init__.py
@@ -1,1 +1,8 @@
 __author__ = 'Jwely'
+
+from build_tex_appendix_figs import *
+from build_tex_tables import *
+from process_piv_experiments import *
+from synthesize_uncertainty_test_images import *
+from test_piv_dynamic_plots import *
+from test_piv_plots import *
