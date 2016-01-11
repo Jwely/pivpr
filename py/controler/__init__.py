bbc4b2be71ad3d1618870310a7e5af96aa207101
Jeff Ely <jeff.ely.08@gmail.com>
2016-01-11 17:34:16 -0500

    forgot to commit init after refactor

--- py/controler/__init__.py
+++ py/controler/__init__.py
@@ -3,6 +3,6 @@ __author__ = 'Jwely'
 from build_tex_appendix_figs import *
 from build_tex_tables import *
 from process_piv_experiments import *
-from synthesize_uncertainty_test_images import *
+from synthesize_piv_uncertainty_images import *
 from test_piv_dynamic_plots import *
 from test_piv_plots import *
