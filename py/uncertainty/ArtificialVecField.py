fc427af86c41cebd55671ab9adf12a9b99336823
Jeff Ely <jeff.ely.08@gmail.com>
2016-01-11 19:37:27 -0500

    artificial vec field plot histogram function
    returns all the attributes of interest for gauging error and uncertainty

--- py/uncertainty/ArtificialVecField.py
+++ py/uncertainty/ArtificialVecField.py
@@ -15,15 +15,53 @@ class ArtificialVecField(VecFieldCartesian):
         self.piv_params = json.loads(open(params_filepath).read())
 
 
-    def plot_histogram(self, component):
+
+    def plot_histogram(self, component, title=None, outpath=None):
 
         data = self[component].flatten()
-        plt.hist(data[~data.mask], bins=30, align="left", color="darksalmon", normed=True)
-        plt.axvline(self.piv_params[component.lower()], color="navy", linestyle="dashed", linewidth=2)
-        plt.show()
+
+        results = {"top": np.nanpercentile(data.filled(np.nan), 99.85),
+                   "bot": np.nanpercentile(data.filled(np.nan), 0.15),
+                   "mean": np.mean(data),
+                   "sim": self.piv_params[component.lower()]}
+
+        results.update({"top_er": 100 * (results['top'] - results['sim']) / results['sim'],
+                        "bot_er": 100 * (results['bot'] - results['sim']) / results['sim'],
+                        "mean_er": 100 * (results['mean'] - results['sim']) / results['sim']})
+
+        # create the histogram
+        fig = plt.figure(figsize=(10, 5), dpi=120, facecolor='w', edgecolor='k')
+        plt.hist(data[~data.mask], bins=100, align="left", color="grey", alpha=0.4)
+
+        # actual velocity line
+        plt.axvline(results["sim"], color="navy",
+                    linestyle="-", linewidth=1, label="Simulated")
+
+        # percentile and average lines
+        plt.axvline(results["mean"], color="navy", linestyle="--", linewidth=1, label="Measured Mean")
+        plt.axvline(results["top"], color="navy", linestyle=":", linewidth=1, label="99.7% Confidence")
+        plt.axvline(results["bot"], color="navy", linestyle=":", linewidth=1)
+
+        plt.xlabel("${0}$  $(m/s)$".format(component.lower()))
+        plt.ylabel("Number of Occurrences")
+        plt.legend(loc=2)
+
+        if title is not None:
+            plt.title(title)
+
+        if outpath:
+            plt.savefig(outpath)
+            print("saved figure to {0}".format(outpath))
+            plt.close()
+        else:
+            plt.show(fig)
+
+        return results
 
 
 if __name__ == "__main__":
     avf = ArtificialVecField("artificial_images/loc-1-high_x/loc-1-high_x.v3d",
                              "artificial_images/loc-1-high_x/params.json",)
-    avf.plot_histogram('U')
+    #avf.plot_histogram('U')
+    #avf.plot_histogram('V')
+    print avf.plot_histogram('W', "Histogram of $w$")
