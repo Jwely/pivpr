1e652b21d38c8768db88eb0e9af1310804686ad5
Jeff Ely <jeff.ely.08@gmail.com>
2016-01-18 21:45:59 -0500

    fixed some errors and added features in AVF

--- py/uncertainty/ArtificialVecField.py
+++ py/uncertainty/ArtificialVecField.py
@@ -21,10 +21,15 @@ class ArtificialVecField(VecFieldCartesian):
         :param params_filepath: filepath to params.json file
         :return:
         """
-        VecFieldCartesian.__init__(self, v3d_path)
         self.piv_params = json.loads(open(params_filepath).read())
+        VecFieldCartesian.__init__(self, v3d_path, velocity_fs=self.piv_params['w'])
+        self.error_data = {}
 
-    def get_error(self, component):
+        # correction factor for directional convention in Y direction
+        self.vel_matrix['V'] *= -1
+
+
+    def get_error(self, component, n_measurements):
 
         # compile some numerical results from the data to return.
         data = self[component].flatten()
@@ -32,35 +37,51 @@ class ArtificialVecField(VecFieldCartesian):
                    "bot": np.nanpercentile(data.filled(np.nan), 2.5),
                    "mean": np.mean(data),
                    "sim": self.piv_params[component.lower()]}
-        results.update({"top_er": 100 * (results['top'] - results['sim']) / results['sim'],
-                        "bot_er": 100 * (results['bot'] - results['sim']) / results['sim'],
-                        "mean_er": 100 * (results['mean'] - results['sim']) / results['sim']})
-        results.update({"max_er": max(results["top_er"], abs(results["bot_er"]))})
+
+        # update results to be in terms of bias and precision for a total uncertainty
+        num = data.count()
+        delta = data - self.piv_params[component.lower()]
+        bias = np.mean(delta)
+        stdev = np.ma.std(data)
+        precision = stdev * 2 / (n_measurements ** 0.5)
+        uncertainty = (bias ** 2 + precision ** 2) ** 0.5
+
+        results.update({"top_dif": results['top'] - results['sim'],
+                        "bot_dif": results['bot'] - results['sim'],
+                        "bias": bias,
+                        "stdev": stdev,
+                        "precision": precision,
+                        "uncertainty": uncertainty})
+
+        self.error_data[component] = results
         return results
 
 
-    def plot_histogram(self, component, title=None, outpath=None):
+    def plot_histogram(self, component, n_measurements=200.0, title=None, outpath=None):
         """
         Creates a histogram of the vectors within the artificial vector field
         and compares it against the parameters used to generate the vector field.
         Thus, bias and random error specific to the parameters used to generate
         the artificial PIV images can be obtained.
 
-        :param component:   either 'U', 'V', or 'W'
-        :param title:       custom title for the plot
-        :param outpath:     filepath to save the figure to.
+        :param component:       either 'U', 'V', or 'W'
+        :param n_measurements:  The number of measurements taken to determine undertainty (usually 200)
+        :param title:           custom title for the plot
+        :param outpath:         filepath to save the figure to.
         :return:
         """
 
-        figsize = (10, 5)    # hardcoded for now
+        figsize = (8, 2.5)    # hardcoded for now
 
         # compile some numerical results from the data to return.
         data = self[component].flatten()
-        results = self.get_error(component)
+        results = self.get_error(component, n_measurements)
 
         # create the histogram
         fig = plt.figure(figsize=figsize, dpi=120, facecolor='w', edgecolor='k')
-        plt.hist(data[~data.mask], bins=100, align="left", color="grey", alpha=0.4)
+        plt.hist(data[~data.mask], bins=150, align="left", color="grey", alpha=0.4)
+        plt.xlim(results['mean'] - 3 * results['stdev'],
+                 results['mean'] + 4 * results['stdev'])
 
         # actual velocity line
         plt.axvline(results["sim"], color="navy",
@@ -73,7 +94,8 @@ class ArtificialVecField(VecFieldCartesian):
 
         plt.xlabel("${0}$  $(m/s)$".format(component.lower()))
         plt.ylabel("Number of Occurrences")
-        plt.legend(loc=2)
+        plt.legend(loc=0)
+        plt.tight_layout()
 
         if title is not None:
             plt.title(title)
@@ -89,8 +111,9 @@ class ArtificialVecField(VecFieldCartesian):
 
 
 if __name__ == "__main__":
-    avf = ArtificialVecField("artificial_images/loc-1-high_x/loc-1-high_x.v3d",
-                             "artificial_images/loc-1-high_x/params.json",)
-    #avf.plot_histogram('U')
-    #avf.plot_histogram('V')
-    print avf.plot_histogram('W', "Histogram of $w$")
+    avf = ArtificialVecField("artificial_images/Ely_May28th07001.v3d",
+                             "artificial_images/Ely_May28th07001.json",)
+
+    print avf.plot_histogram('U')
+    print avf.plot_histogram('V')
+    print avf.plot_histogram('W', 200, title="Histogram of $w$")
