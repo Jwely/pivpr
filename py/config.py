eee41bcaa9715a5b5f699029292baf7aa182162a
Jeff Ely <jeff.ely.08@gmail.com>
2016-01-19 22:13:59 -0500

    changed to greyscale

--- py/config.py
+++ py/config.py
@@ -32,9 +32,9 @@ DEFAULT_MIN_POINTS = 20             # minimum number of points required to consi
 
 # global plotting variables
 CONTOUR_DEFAULT_LEVELS = 32         # number of discreet colors to use on contour plots
-CONTOUR_DEFAULT_CMAP = cm.jet     # default color ramp to use on contour plots (greyscale friendly)
+CONTOUR_DEFAULT_CMAP = cm.Greys     # default color ramp to use on contour plots (greyscale friendly)
 CONTOUR_DEFAULT_RRANGE = (0, 100)   # default radius range to subset contour plots by
-SCATTER_DEFAULT_CMAP = cm.jet     # default colormap to use on scatter plots with third variable
+SCATTER_DEFAULT_CMAP = cm.Greys     # default colormap to use on scatter plots with third variable
 SCATTER_DEFAULT_RLIM = 100          # default plot
 VRANGE_DEFAULT = (1, 99.9)          # default percentile values for defining color ramp boundaries
 
