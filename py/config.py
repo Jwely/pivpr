42b0de29c597b90fd89383e51d8205678c3e55ea
Jeff Ely <jeff.ely.08@gmail.com>
2016-01-27 23:49:11 -0500

    tweaks

--- py/config.py
+++ py/config.py
@@ -33,12 +33,12 @@ DEFAULT_MIN_POINTS = 20             # minimum number of points required to consi
 
 
 # global plotting variables
-CONTOUR_DEFAULT_LEVELS = 32         # number of discreet colors to use on contour plots
+CONTOUR_DEFAULT_LEVELS = 256        # number of discreet colors to use on contour plots
 CONTOUR_DEFAULT_CMAP = cm.Greys     # default color ramp to use on contour plots (greyscale friendly)
-CONTOUR_DEFAULT_RRANGE = (0, 100)   # default radius range to subset contour plots by
+CONTOUR_DEFAULT_RRANGE = (0, 50)   # default radius range to subset contour plots by
 SCATTER_DEFAULT_CMAP = cm.Greys     # default colormap to use on scatter plots with third variable
 SCATTER_DEFAULT_RLIM = 100          # default plot
-VRANGE_DEFAULT = (1, 99.9)          # default percentile values for defining color ramp boundaries
+VRANGE_DEFAULT = (1, 99)          # default percentile values for defining color ramp boundaries
 
 # attributes of the dataset
 SAMPLING_RATE = 1                   # the sampling rate in Hz. (1/time between vector fields)
