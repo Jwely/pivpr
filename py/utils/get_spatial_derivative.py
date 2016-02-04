6fec98f529e6ce3abb025145e68cfd9951b3cfa1
Jeff Ely <jeff.ely.08@gmail.com>
2016-02-04 14:36:25 -0500

    figured out cylindrical partial spatial derivatives
    the thing i tried first was correct, but was testing it on small
    matrices what were disrupted by edge effects and non regular events.

--- py/utils/get_spatial_derivative.py
+++ py/utils/get_spatial_derivative.py
@@ -1,9 +1,10 @@
 __author__ = "Jwely"
 
 import numpy as np
+import math
 
 
-def divide_by_zero(a, b, mask):
+def dbz(a, b):
     """
     divides two nd arrays element wise, but returns 0 if either denomenator
     or numerator is zero.
@@ -11,20 +12,15 @@ def divide_by_zero(a, b, mask):
     :param b:  the denominator
     :return:   a / b
     """
-    if isinstance(a, np.ma.MaskedArray):
-        a = a.data
-    if isinstance(b, np.ma.MaskedArray):
-        b = b.data
-    with np.errstate(divide='ignore', invalid='ignore'):
-        c = np.true_divide(a, b)
-        c[c == np.inf] = 0
-        c[np.isnan(c)] = 0
-        c = np.nan_to_num(c)
-        c = np.ma.masked_array(c, mask=mask)
-    return c
+    q = a / b
+    q[q > 1e100] = 0
+    q[q == np.nan] = 0
+    q[q == -np.inf] = 0
+    q[q == np.inf] = 0
+    return q
 
 
-def get_spatial_derivative_cartesian(f, x, y):
+def get_spatial_derivative(f, x, y):
     """
     Returns a derivative of f with respect to x and y for discreet nd arrays of points.
     :param f:   2d array of some function f values
@@ -41,60 +37,31 @@ def get_spatial_derivative_cartesian(f, x, y):
     dxdi, dxdj = np.gradient(x)
     dydi, dydj = np.gradient(y)
 
-    dfdx = divide_by_zero(dfdj, dxdj, mask) + divide_by_zero(dfdi, dxdi, mask)
-    dfdy = divide_by_zero(dfdi, dydi, mask) + divide_by_zero(dfdj, dydj, mask)
+    dfdx = dbz(dfdj, dxdj) + dbz(dfdi, dxdi)
+    dfdy = dbz(dfdi, dydi) + dbz(dfdj, dydj)
 
     return dfdx, dfdy
-
-
-def get_spatial_derivative_cylindrical(f, r, t):
-    """
-    Returns a derivative of f with respect to r and theta for discreet nd arrays of points.
-    :param f:   2d array of some function f values
-    :param r:   2d array of radius coordinates (radius)
-    :param t:   2d array of tangential coordinates (theta)
-    """
-
-    mask = np.zeros(f.shape)
-    if isinstance(f, np.ma.MaskedArray):
-        mask = f.mask
-
-    # derivatives in terms of index locations i,j
-    dfdi, dfdj = np.gradient(f)
-    drdi, drdj = np.gradient(x)
-    dtdi, dtdj = np.gradient(y)
-
-    dfdr = divide_by_zero(dfdj, drdj, mask) + divide_by_zero(dfdi, drdi, mask)
-    dfdt = divide_by_zero(dfdj, dtdj, mask) + divide_by_zero(dfdi, dtdi, mask)
-
-    return dfdr, dfdt
-
-
-def get_spatial_derivative(f, a, b, cylindrical=False):
-
-    if cylindrical:
-        return get_spatial_derivative_cylindrical(f, a, b)
-    else:
-        return get_spatial_derivative_cartesian(f, a, b)
 
 
 # testing area
 if __name__ == "__main__":
 
-    xl = np.linspace(1, 6, 6)
-    yl = np.linspace(1, 6, 6)
+
+    # test out both cylindrical and cartesian
+    xl = np.linspace(1, 100, 100)
+    yl = np.linspace(1, 100, 100)
     x, y = np.meshgrid(xl, yl)
-    r = ((x + 10.5) ** 2 + (y + 10.5) ** 2) ** 0.5
-    t = np.arctan(y / x)
+    xi, yi = (1.1, 1.1)
+    x -= xi
+    y -= yi
 
-    f1 = 3 * x + 4 * y
-    dfdx, dfdy = get_spatial_derivative(f1, x, y)
-    #print dfdx
-    #print dfdy
+    r = ((x) ** 2 + (y) ** 2) ** 0.5
+    t = np.arctan2(y, x)
 
-    f2 = 2*t
-    dfdr, dfdt = get_spatial_derivative(f2, r, t, cylindrical=True)
-    print r
-    print t
-    print dfdr
-    print dfdt
+    f = 5 * r + 6 * t
+    dfdx, dfdy = get_spatial_derivative(f, x, y)
+    dfdr = (dbz(dfdy, np.sin(t)) + dbz(dfdx, np.cos(t))) / 2
+    dfdt = (dbz(dfdy, (1/r) * np.cos(t)) + dbz(dfdx, (-1/r) * np.sin(t))) / 2
+    print np.percentile(dfdr, 50)
+    print np.percentile(dfdt, 50)
+
