86c5519bea52958b310deb54c8e644f064c34393
Jeff Ely <jeff.ely.08@gmail.com>
2016-02-06 21:20:44 -0500

    refactor

--- py/controler/__init__.py
+++ py/controler/__init__.py
@@ -1,6 +1,6 @@
 __author__ = 'Jwely'
 
-from build_piv_tex_figures import *
+from build_tex_figs_by_run import *
 from build_tex_tables import *
 from synthesize_piv_uncertainty_images import *
 from test_piv_dynamic_plots import *
