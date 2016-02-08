ab06fba84e7f62d22b59661f34e9bdb324d76196
Jeff Ely <jeff.ely.08@gmail.com>
2016-02-08 15:51:36 -0500

    added dynamic plots to primary controller

--- py/controler/build_tex_figs_by_run.py
+++ py/controler/build_tex_figs_by_run.py
@@ -7,7 +7,7 @@ from py.config import *
 from py.utils import merge_dicts
 
 
-def build_tex_figs_by_run(run_id, include_cartesian=False, force_recalc=False):
+def build_tex_figs_by_run(run_id, include_cartesian=False, include_dynamic=False, force_recalc=False):
     """
     Very similar to build_appendix_figs_by_run, but this function creates a separate
     tex document for every figure to include in the discussion section where lots of text will
@@ -24,7 +24,7 @@ def build_tex_figs_by_run(run_id, include_cartesian=False, force_recalc=False):
                                 ids=[run_id],
                                 min_points=DEFAULT_MIN_POINTS,
                                 force_recalc=force_recalc,
-                                include_dynamic=False)[0]
+                                include_dynamic=include_dynamic)[0]
     tfp = TeXRunFigurePage(TEX_MAIN_PATH, "run_{0}".format(run_id), exp, force_recalc=force_recalc)
 
     av = exp.axial_vortex
@@ -71,7 +71,6 @@ def build_tex_figs_by_run(run_id, include_cartesian=False, force_recalc=False):
     tfp.add_scatter_plot('r_mesh', 'ctke', caption, scatter_width, create_kwargs=k_kwargs, write_unique=True)
 
 
-
     # logarithmic plots with kwqargs
     log_kwargs = merge_dicts(scatter_kwargs, {"log_y": True,
                                               "y_range": (1e2, 1e9),
@@ -89,6 +88,17 @@ def build_tex_figs_by_run(run_id, include_cartesian=False, force_recalc=False):
         z_location, av.velocity_fs, station_id)
     tfp.add_scatter_plot('r_mesh', 'turb_visc_ettap_bot', caption, scatter_width, create_kwargs=kwargs, write_unique=True)
 
+
+    # include dynamic plots
+    if include_dynamic:
+        dynamic_kwargs = {"r_range": ('0r', '1r')}
+        caption = "Plot showing dynamic variations in turbulent kinetic energy within the core."
+        tfp.add_dynamic_plot('ctke', caption, scatter_width, special_tag="01r", create_kwargs=dynamic_kwargs, write_unique=True)
+
+        dynamic_kwargs = {"r_range": ('1r', '2r')}
+        caption = "Plot showing dynamic variations in turbulent kinetic energy between 1 and 2 core radii"
+        tfp.add_dynamic_plot('ctke', caption, scatter_width, special_tag="12r", create_kwargs=dynamic_kwargs, write_unique=True)
+
     # and write the appendix index file with all of the plots.
     tfp.write()
 
@@ -96,11 +106,12 @@ def build_tex_figs_by_run(run_id, include_cartesian=False, force_recalc=False):
 def main():
     # build tex figs for all trials
     run_ids = range(1, 71)
+    run_ids = [35, 45, 55]
     for run_id in run_ids:
-        build_tex_figs_by_run(run_id, force_recalc=False)
+        build_tex_figs_by_run(run_id, include_dynamic=True, force_recalc=False)
 
     # include cartesian coordinate tex figs for example run number 55
-    build_tex_figs_by_run(55, include_cartesian=True)
+    #build_tex_figs_by_run(55, include_cartesian=True)
 
 
 
