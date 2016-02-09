db25289abcab5a07e16cfbb3c44b97e9ca579d1e
Jeff Ely <jeff.ely.08@gmail.com>
2016-02-09 00:29:17 -0500

    line break

--- py/controler/build_tex_figs_by_run.py
+++ py/controler/build_tex_figs_by_run.py
@@ -103,7 +103,8 @@ def build_tex_figs_by_run(run_id, include_cartesian=False, include_dynamic=False
         z_location, av.velocity_fs, station_id)
     tfp.add_scatter_plot('r_mesh', 'turb_visc_ettap_bot', caption, scatter_width, create_kwargs=kwargs, write_unique=True)
 
-    kwargs = merge_dicts(log_kwargs, {"title": r"$\eta_P \bar{t}[\overline{r^\prime r^\prime} - \overline{t^\prime t^\prime} + \frac{d(\overline{r^\prime r^\prime})}{dr}]$"})
+    kwargs = merge_dicts(log_kwargs, {"title": r"$\eta_P \bar{t}[\overline{r^\prime r^\prime} - \overline{t^\prime t^\prime}"
+                                               r" + \frac{d(\overline{r^\prime r^\prime})}{dr}]$"})
     kwargs['y_range'] = (1e-5, 1e3) # much much much smaller.
     caption = "Scatter plot of $\nu_T$ pressure relaxation term vs radius at $z/c$={0}, $V_{{free}}$={1}, station {2}.".format(
         z_location, av.velocity_fs, station_id)
