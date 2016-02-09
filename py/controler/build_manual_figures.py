27546618eaa0114056ab10f29d2a4f2831752f33
Jeff Ely <jeff.ely.08@gmail.com>
2016-02-09 14:47:34 -0500

    template manual fig building controler
    there are a few figures that may need to be produced that really come
    from manual datasets, for consistency it would be good to also render
    these plots with matplotlib. This is a stand alone controller for those
    kinds of plots.

---
+++ py/controler/build_manual_figures.py
@@ -0,0 +1,19 @@
+__author__ = "Jwely"
+
+
+
+def build_manual_figures():
+    """
+    There are some plots based on external datasets that need to be built manually.
+    This is done here.
+    :return:
+    """
+    pass
+
+
+
+def _tke_thompson_comparison():
+    """
+    compares turbulent kinetic energy values measured by thompson to mine.
+    """
+    pass
