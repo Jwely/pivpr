cdb6ef4a5db8aefb148a931f22363a78b38cb8c6
Jeff Ely <jeff.ely.08@gmail.com>
2016-02-11 16:59:53 -0500

    table test updates with notation change

--- py/controler/build_tex_figs_by_run.py
+++ py/controler/build_tex_figs_by_run.py
@@ -152,9 +152,9 @@ def build_tex_figs_by_run(run_id, include_cartesian=False, include_dynamic=False
 def main():
     # build tex figs for all trials
     run_ids = range(1, 71)
-    #run_ids = [55]
+    run_ids = [55]
     for run_id in run_ids:
-        build_tex_figs_by_run(run_id, include_dynamic=True, force_recalc=True)
+        build_tex_figs_by_run(run_id, include_dynamic=True, force_recalc=False)
 
     # include cartesian coordinate tex figs for example run number 55
     build_tex_figs_by_run(55, include_cartesian=True)
