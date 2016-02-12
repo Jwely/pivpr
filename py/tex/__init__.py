f7de206390c23dccd86adabff17ba58f7e7275c4
Jeff Ely <jeff.ely.08@gmail.com>
2016-02-12 10:39:31 -0500

    fixed imports

--- py/tex/__init__.py
+++ py/tex/__init__.py
@@ -3,3 +3,4 @@ __author__ = 'Jwely'
 from TeXFigureGenerator import *
 from TeXRunFigurePage import *
 from TeXWriter import *
+from csv_to_tex import *
--- py/utils/__init__.py
+++ py/utils/__init__.py
@@ -1,12 +1,12 @@
 __author__ = 'Jeff'
 
+from Timer import *
 from cart2cyl_vector import *
-from csv_to_tex import *
-from install_dependencies import *
 from get_rel_humidity import *
 from get_spatial_derivative import *
-from masked_rms import *
+from install_dependencies import *
 from masked_mean import *
+from masked_rms import *
 from merge_dicts import *
 from smooth_filt import *
-from Timer import *
+
